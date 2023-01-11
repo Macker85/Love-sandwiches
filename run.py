e97c669fa8e9ee4ee2e6b25b6a43032e7e66c26a
Macker85 <pmacker1985@gmail.com>
2023-01-11 10:24:36 +0000

    Create get_saless_data function to get string from user

--- run.py
+++ run.py
@@ -12,8 +12,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Copy of love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Definition here
+    """
+    print("enter dales data")
+    print("data should be 6 numners, seperated by commas")
+    print("example:10,20,30,40,50,60\n")
 
-print(data)
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+
+get_sales_data()
